6b1069b4699568af2f72ba12f1148b85d0a8dda9
Marten <131295106+Marten235@users.noreply.github.com>
2024-05-02 19:36:07 +0300

    Update Untitled-1.cpp

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,50 +1,87 @@
 #include <iostream>
 using namespace std;
-#include <vector>
 
 
-void print(vector<int> arr)
+int numberofmoves (int R , int L,int list[8][8])
 {
-  for(int i=0 ; i<arr.size(); i++)
+  int numberofmoves = 0;
+  for (int i = -2 ; i <=2 ; i++)
   {
-    cout << arr[i]<<",";
-  }
-  cout<<"\n";
-}
-
-int runningTime(vector<int> arr) 
-{
-  int x=0,count=0,e=0;
-  for(int i=1 ; i<arr.size(); i++)
-  { e=i;
-    for(int j = i-1 ; j>=0;j--)
+    for(int j = -2 ; j<=2 ; j++)
+    {
+      if (i == 0 || j == 0 || abs(i) == abs(j) )continue;
+      if (R+i < 8 && L+j < 8 && R+i >= 0 && L+j >= 0 && list[R+i][L+j]==0)
       {
-       
-        if(arr[j]>arr[e])
-        {
-          x=arr[j];
-          arr[j]=arr[e];
-          arr[e]=x;
-          count++;
-          e--;
-        }
+        numberofmoves++;
       }
+    }
   }
-  return count;
+  return numberofmoves;
 }
 
+void printl(int list[8][8])
+{
+  for (int i = 0; i < 8; i++)
+  {
+    for (int j = 0; j < 8; j++)
+    {
+      cout<<list[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+  cout<<"-------------------------------------------------------------------"<<endl;
+}
 
 int main()
 {
-  vector<int>arr;
-  arr.push_back(4);
-  arr.push_back(4);
-  arr.push_back(3);
-  arr.push_back(4);
+  int list[8][8] = {0};
+  int R=0,L=0; // Strating point
+  list[R][L]=1;
+  int oldL=L , oldR=R;
 
- 
 
-  print(arr);
-  cout <<runningTime(arr);
-   
+
+
+  for (int k = 0; k < 100; k++)
+  {
+    int minNOM = 10,newR ,newL,NOM ; 
+    for (int i = -2 ; i <=2 ; i++)
+    {
+      for(int j = -2 ; j<=2 ; j++)
+      {
+        if (i == 0 || j == 0 || abs(i) == abs(j) )continue;
+        if (R+i < 8 && L+j < 8 && R+i >= 0 && L+j >= 0 && list[R+i][L+j]==0)
+        {
+          NOM=numberofmoves(R+i, L+j,list);
+          if (NOM < minNOM)
+          {
+            minNOM = NOM;
+            newR = R+i;
+            newL = L+j;
+          }
+        }
+
+      }
+    
+
+    }
+    oldR = R;
+    oldL=L;
+    R=newR;
+    L=newL;
+    list[R][L]=1;
+  
+      
+
+
+    printl(list);
+    if (minNOM ==0)
+    {R=oldR; L=oldL; break;}
+    
+
+  }
+
+  
+
+
 }
